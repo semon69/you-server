8806d13d6a2fe33047a4ea72253a4da6c509bca0
semon69 <smdemon3@gmail.com>
2023-10-23 12:58:29 +0600

    connect to mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
+require('dotenv').config()
 const port = process.env.PORT || 5000
 
 
@@ -10,6 +11,38 @@ app.use(express.json())
 app.get("/", (req, res)=> [
     res.send("You is Running")
 ])
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kyulyzl.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    
+
+
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, ( )=> {
     console.log(`You social media running on port ${port}`);
